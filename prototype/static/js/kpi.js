806ea97cd449da29ff441b30da5b9f77f973df3d
Arik100 <arik@europe.com>
2016-04-18 20:56:04 +0300

    Update 2 kpi.js
    1. Display only indicator labels;
    2. Įvykių atvaizdavimas grafike.

--- prototype/static/js/kpi.js
+++ prototype/static/js/kpi.js
@@ -1,8 +1,9 @@
-(function (d3) {
+(function (d3, c3) {
     d3.json("/topic/balsavimas-internetu/kpi/", function(jsonData) {
-        var i = 1;
-        var yLabel;
-        var dataToChart = [], dataNames = [], yAxe = [], dataAndX = [], chartType = [];
+        var i = 1; 
+        var yLabel, eventsLevel, eventsLevelSameDay, tempDate, tempLevel;
+        var dataMin = Number.MAX_VALUE, dataMax = Number.MIN_VALUE;
+        var dataToChart = [], dataNames = [], dataAndX = [], chartType = [], dataHide = [];
 
         jsonData.indicators.forEach(function(jsonEachData) {
             var itemName = "data" + i;
@@ -11,14 +12,17 @@
             dataAndX[itemName] = date[0];
             dataNames[itemName] = jsonEachData.title;
             chartType[itemName] = "line";
-            yAxe[itemName] = 'y';
             yLabel = jsonEachData.ylabel;
             jsonEachData.data.forEach(function(dateAndMark) {
                 dateAndMark.forEach(function(dateOrMark, j) {
-                    if (j % 2 == 0) 
-                        date.push(dateOrMark)
-                    else 
+                    if (j % 2 == 0) {
+                        date.push(dateOrMark);
+                    } 
+                    else {
                         mark.push(dateOrMark);
+                        dataMin = Math.min(dataMin, dateOrMark);
+                        dataMax = Math.max(dataMax, dateOrMark);
+                    }   
                 });
             });
             dataToChart.push(date);
@@ -26,33 +30,40 @@
             i++;
         });
 
+        eventsLevel = dataMin - (dataMax - dataMin) * 0.1;
+        eventsLevelSameDay = (dataMax - dataMin) * 0.02;
+
         jsonData.events.forEach(function(jsonEachData) {
             var itemName = "data" + i;
             var date = ["x"+i];
             var mark = [itemName];
-            var temporarMark; 
             dataAndX[itemName] = date[0];
             dataNames[itemName] = jsonEachData.title;
             chartType[itemName] = "scatter";
-            yAxe[itemName] = 'y2';
             date.push(jsonEachData.date);
-            temporarMark = jsonEachData.position;
-            if (temporarMark == null) 
-                temporarMark = 0;
-            mark.push(temporarMark);
+            if (tempDate == jsonEachData.date) {
+                tempLevel += eventsLevelSameDay;
+            }
+            else {
+                tempLevel = eventsLevel;
+            }
+            tempDate = jsonEachData.date;
+            mark.push(tempLevel);
             dataToChart.push(date);
             dataToChart.push(mark);
+            dataHide.push(itemName);
             i++;
         });
 
-        // window.alert(dataToChart);
-        var chart = c3.generate( {	
+        c3.generate( {
             data: {
                 xs: dataAndX,
                 columns: dataToChart,
                 names: dataNames,
-                types: chartType,
-                axes: yAxe,
+                types: chartType
+            },
+            legend: {
+                hide: dataHide
             },
             axis: {
                 x: {
@@ -67,11 +78,8 @@
                         text: yLabel,
                         position: 'outer-middle'
                     }
-                },
-                y2: {
-                    show: true
                 }
             }
-        });	
+        });
     });
-}(d3));
+}(d3, c3));  //eslint-disable-line no-undef
