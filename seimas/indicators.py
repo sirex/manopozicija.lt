331b69636b9d6a85ce7828114dff126cf45f2e1c
Mantas <sirexas@gmail.com>
2016-05-18 17:55:35 +0300

    Add first voter turnout indicator
    See: https://trello.com/c/WL7a4CrF/39-kpi-importavimas-ir-administravimas

---
+++ seimas/indicators.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def voter_turnout():
+    frame = pd.read_csv(
+        'http://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?file=data/tsdgo310.tsv.gz',
+        sep='\t',
+        compression='gzip',
+        index_col=0,
+        na_values=[': '],
+    )
+
+    frame = (
+        frame.stack().
+        loc['NAT_VOTE,LT', :].to_frame().
+        reset_index(level=0, drop=True)
+    )
+
+    frame.index = pd.to_datetime(frame.index.str.strip(), format='%Y')
+    frame[0] = frame[0].astype(float)
+    frame = frame.rename(columns={0: 'Seimo'})
+
+    return frame, {
+        'title': 'Rinkimuose dalyvavusių rinkėjų skaičius, palyginti su visų rinkėjų skaičiumi',
+        'ylabel': 'Aktyvumas procentais',
+    }
+
+
+INDICATORS = [
+    ('voter_turnout', 'seimas.indicators.voter_turnout'),
+]
