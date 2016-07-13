b939211e6bc4aec5835da817085111cad8d45c22
Mantas <sirexas@gmail.com>
2016-07-13 14:58:28 +0300

    Event post design in topic post feed

--- manopozicija/helpers.py
+++ manopozicija/helpers.py
@@ -1,29 +1,53 @@
-def _get_source_position_image(position):
+def _get_position_image(position, positive, negative, neutral):
     if position > 0.5:
-        return 'img/actor-positive.png'
-    elif position < 0.5:
-        return 'img/actor-negative.png'
+        return positive
+    elif position < -0.5:
+        return negative
     else:
-        return 'img/actor-neutral.png'
+        return neutral
 
 
 def get_posts(posts):
     result = []
     for post in posts:
         if post['type'] == 'event':
-            result.append(post)
+            event = post['event']
+            result.append({
+                'type': 'event',
+                'event': {
+                    'position_image': _get_position_image(
+                        event.position,
+                        'img/event-positive.png',
+                        'img/event-negative.png',
+                        'img/event-neutral.png',
+                    ),
+                    'name': event.title,
+                    'timestamp': event.timestamp.strftime('%Y-%m-%d'),
+                    'source': {
+                        'link': event.source_link,
+                        'name': event.source_title,
+                    },
+                    'upvotes': post['post'].upvotes,
+                },
+            })
         else:
             source = post['source']
             actor = source.actor
             result.append({
+                'type': 'quotes',
                 'source': {
                     'link': source.source_link,
-                    'title': source.source_title,
+                    'name': source.source_title,
                     'actor': {
                         'name': str(actor),
                         'title': source.actor_title or actor.title,
                         'photo': actor.photo,
-                        'position_image': _get_source_position_image(source.position),
+                        'position_image': _get_position_image(
+                            source.position,
+                            'img/actor-positive.png',
+                            'img/actor-negative.png',
+                            'img/actor-neutral.png',
+                        ),
                     },
                 },
                 'quotes': [{
