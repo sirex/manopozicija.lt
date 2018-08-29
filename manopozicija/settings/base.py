3dfb242ff636857f42077a95643fb918220cf284
Mantas <sirexas@gmail.com>
2018-08-29 15:42:36 +0300

    Fix some issues missed by automated tests

--- manopozicija/settings/base.py
+++ manopozicija/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS += (
 )
 
 MIDDLEWARE = (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- manopozicija/urls.py
+++ manopozicija/urls.py
@@ -35,6 +35,6 @@ urlpatterns += [
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += [path('__debug__/', debug_toolbar.urls)]
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
